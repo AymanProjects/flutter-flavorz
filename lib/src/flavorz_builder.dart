db8ff3c9a6531a06557cbcb995a607b019f26d66
ikema77 <brhoom.kh@gmail.com>
2022-08-19 10:22:42 +0300

    Fixing escape issue

--- lib/src/flavorz_builder.dart
+++ lib/src/flavorz_builder.dart
@@ -112,7 +112,7 @@ ${_generateAttributes(flavor)}
       _this = matchedEnvironments.first;
     } else {
       throw Exception(
-          'The environment that you wish to run doesn't exist in \$pathToJsonConfigFile');
+          'The environment that you wish to run does not exist in \$pathToJsonConfigFile');
     }
   }
 
